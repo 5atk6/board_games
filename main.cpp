4689c622f85809156f7d35edfb0628b173868234
Atsuki Iwai <atksuimiy656@gmail.com>
2021-05-28 21:31:35 +0900

    add check_board function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 #define WHITE -1
 #define BLACK 1
+#define WIN 1
 
 void print_board(int board[9][9])
 {
@@ -30,10 +31,52 @@ void print_board(int board[9][9])
     }
 }
 
+int check_board(int x, int y, int board[9][9])
+{
+    int periphery[3][3] = {0};
+    int ret = 0;
+
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            if (i == 0 && j == 0) {
+                continue;
+            }
+
+            for (int k = 1; k <= 4; k++) {
+                int ix = x + i * k;
+                int iy = y + j * k;
+                if (ix < 0 || ix >= 9 || iy < 0 || iy >= 9) {
+                    continue;
+                }
+
+                if (board[ix][iy] == board[x][y]) {
+                    if (k == 4) {
+                        periphery[i+1][j+1] = k;
+                    }
+                    continue;
+                } else {
+                    periphery[i+1][j+1] = k - 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    int a = periphery[0][0] + periphery[2][2];
+    int b = periphery[1][0] + periphery[1][2];
+    int c = periphery[2][0] + periphery[0][2];
+    int d = periphery[0][1] + periphery[2][1];
+    if (a >= 4 || b >= 4 || c >= 4 || d >= 4) {
+        ret = 1;
+    }
+
+    return ret;
+}
+
 int main()
 {
     int board[9][9] = {0};
-    int x, y;
+    int x, y, ret;
     int player = 1;
 
     while (true) {
@@ -55,8 +98,14 @@ int main()
         if (board[x][y] == 0) {
             board[x][y] = player;
             print_board(board);
+            ret = check_board(x, y, board);
+            if (ret == WIN) {
+                cout << player << " wins!" << endl;
+                break;
+            }
         } else {
             cout << "Please input other place." << endl;
+            continue;
         }
 
         player = player * (-1);
