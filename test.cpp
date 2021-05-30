d175314c09af3ee2fc6cade71652d5a71f1071e0
Atsuki Iwai <atksuimiy656@gmail.com>
2021-05-30 20:58:38 +0900

    add a test code

---
+++ test.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+
+#include "board.h"
+
+TEST(check_board_test, case01) {
+    int board[9][9] = {0};
+    board[0][0] = 1;
+    board[0][1] = 1;
+    board[0][2] = 1;
+    board[0][3] = 1;
+    board[0][4] = 1;
+    EXPECT_EQ(check_board(0, 4, board), 1);
+}
+
+TEST(check_board_test, case02) {
+    int board[9][9] = {0};
+    board[0][0] = 1;
+    board[1][1] = 1;
+    board[2][2] = 1;
+    board[3][3] = 1;
+    board[4][4] = 1;
+    EXPECT_EQ(check_board(4, 4, board), 1);
+}
+
+TEST(check_board_test, case03) {
+    int board[9][9] = {0};
+    board[0][4] = 1;
+    board[1][3] = 1;
+    board[2][2] = 1;
+    board[3][1] = 1;
+    board[4][0] = 1;
+    EXPECT_EQ(check_board(4, 0, board), 1);
+}
