944e6ca3017979ac405a061b7a955a03a76a3583
Atsuki Iwai <atksuimiy656@gmail.com>
2021-06-02 22:43:59 +0900

    make a receive_position function

--- main.cpp
+++ main.cpp
@@ -1,43 +1,49 @@
 #include <iostream>
 #include "board.h"
 
+int player = BLACK;
+
+int receive_position(int x, int y, int board[9][9])
+{
+    int ret = 0;
+    x--;
+    y--;
+
+    if (x < 0 || x >= 9 || y < 0 || y >= 9) {
+        cout << "Please input 1 to 9." << endl;
+        return ret;
+    }
+
+    if (board[x][y] == 0) {
+        board[x][y] = player;
+        print_board(board);
+        ret = check_board(x, y, board);
+        if (ret == WIN) {
+            cout << player << " wins!" << endl;
+            return ret;
+        }
+    } else {
+        cout << "Please input other place." << endl;
+        return ret;
+    }
+
+    player = player * (-1);
+    return ret;
+}
+
 int main()
 {
     int board[9][9] = {0};
-    int x, y, ret;
-    int player = 1;
+    int x, y;
 
     while (true) {
         cin >> x >> y;
 
-        if (x == 0 && y == 0) {
-            cout << "Terminate this game." << endl;
+        if(receive_position(x, y, board) == WIN) {
             break;
         }
-
-        x--;
-        y--;
-
-        if (x < 0 || x >= 9 || y < 0 || y >= 9) {
-            cout << "Please input 1 to 9." << endl;
-            continue;
-        }
-
-        if (board[x][y] == 0) {
-            board[x][y] = player;
-            print_board(board);
-            ret = check_board(x, y, board);
-            if (ret == WIN) {
-                cout << player << " wins!" << endl;
-                break;
-            }
-        } else {
-            cout << "Please input other place." << endl;
-            continue;
-        }
-
-        player = player * (-1);
     }
 
+    cout << "Finish this game." << endl;
     return 0;
 }
