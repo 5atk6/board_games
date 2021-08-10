aa19fab39edefaa0f33b4e3e4a22bd6d907a251a
Atsuki Iwai <atksuimiy656@gmail.com>
2021-08-11 07:48:32 +0900

    refactor: use original words

--- web/web_interface.js
+++ web/web_interface.js
@@ -2,29 +2,29 @@ var http = require('http');
 var html = require('fs').readFileSync('index.html');
 var fs   = require('fs');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
 
   try {
       const board = fs.readFileSync('../build/board.txt', 'utf8');
-      res.write(board);
+      response.write(board);
   } catch (err) {
       console.error(err);
   }
 
   var data = '';
   const command = '../build/client '
-  if (req.method == 'POST') {
-    req.on('data', function(chunk) {data += chunk})
+  if (request.method == 'POST') {
+    request.on('data', function(chunk) {data += chunk})
        .on('end', function() {
            console.log(data);
 
            var x = data.split('&')[0].split('=')[1]
            var y = data.split('&')[1].split('=')[1]
-           res.end(html);
+           response.end(html);
            const { exec } = require('child_process');
            exec(command + x + ' ' + y);
        })
   }
-  res.end(html);
+  response.end(html);
 }).listen(8080); 
